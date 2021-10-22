a079a9c6a1b33c6364d5f5ca6e6d0ec683ad7b18
Hrishikesh Nalawade <80455130+HrishikeshNalawade1@users.noreply.github.com>
2021-10-22 19:21:40 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace pallio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int T,r=0;
+            Console.WriteLine("Enter a number");
+            int num = Convert.ToInt16(Console.ReadLine());
+            int x = num;
+            while(x!=0)
+            {
+                T = x % 10;
+                r = (r*10) +T;
+                x /= 10;
+            }
+            if(r==num)
+            {
+                Console.WriteLine("Its a paliandrome");
+            }
+            else
+            {
+                Console.WriteLine("Its a normal number");
+            }
+        }
+    }
+}
